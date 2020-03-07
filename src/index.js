6b34100b8c6ffa330dacc9c6ebadc271ebf42739
Alexandre LE CORRE <42843594+elreco@users.noreply.github.com>
2020-03-07 22:41:01 +0100

    tests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
-
 const rootElement = document.getElementById("root");
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <h2>Magali Joseph</h2>
+        <h3>Test</h3>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
